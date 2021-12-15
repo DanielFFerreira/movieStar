6b7739fdd690955ba4a3bbb4bfbdbfa778018951
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-12-15 12:09:27 -0300

    criando conexão com banco de dados com pdo

--- app/database/db.php
+++ app/database/db.php
@@ -9,4 +9,5 @@
   $conn = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 
   // habilitar os erros no PDO.
-  // $conn->setAttribute();
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
