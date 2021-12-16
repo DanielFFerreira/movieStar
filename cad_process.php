848851c36c434710584f97d2672e4c28a988b706
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-12-16 09:21:41 -0300

    capturando tipo do input para a validação

--- cad_process.php
+++ cad_process.php
@@ -1,3 +1,33 @@
 <?php
 
-  echo "Olá";
+  // inserindo uma vez o arquivo globals
+  require_once("config/globals.php");
+  // inserir o arquivo de conexão com o banco de dados
+  require_once("app/database/db.php");
+  // logar usuarios
+  require_once("models/User.php");
+  require_once("config/dao/UserDAO.php");
+
+  // resgata o tipo do formulário
+  $type = filter_input(INPUT_POST, "type");
+
+  // verificação do tipo de formulário
+  if($type === "registrar") {
+
+    $name = filter_input(INPUT_POST, "name");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
+    $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+
+    // verificação(validar) de dados mínimos
+    if($name && $lastname && $email && $password) {
+
+    }else {
+      // enviar uma msg de erro, dos dados faltantes
+      
+    }
+
+  }else if($type === "login") {
+
+  }
