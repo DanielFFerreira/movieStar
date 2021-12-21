49af6325479bc1bd97176915ddb51f7d537ee5b6
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-12-21 10:55:24 -0300

    usuário não encontrado e limitando tamanho senha

--- auth_process.php
+++ auth_process.php
@@ -30,10 +30,11 @@
       if($password === $confirmpassword) {
         // verificar se o e-mail já está cadastrado no sistema
         if($userDao->findByEmail($email) === false) {
-
+          // mensagem (nenhum usuário foi encontrado)
+          echo "Nenhum usuário foi encontrado!";
         }else {
           // enviar uma msg de erro, usuário já existe
-          $message->setMessage("Usuário já cadastrado. Por favor tente outro e-mail.", "error", "back");
+          $message->setMessage("Usuário já cadastrado, tente outro e-mail.", "error", "back");
         }
 
       }else {
@@ -47,12 +48,14 @@
     }
 
     // verificar o tamanho da senha
-    if(strlen($password >= 12)) {
+    if(strlen($password > 12)) {
       $message->setMessage("A senha ultrapassou o tamanho máximo permitido.", "error", "back");
     }
 
-    // if(strlen($password < 3)) {
-    //   $message->setMessage("A senha têm que ter no mínimo que quatro caracteres.", "error", "back");
+    // if(strlen($password < 1)) {
+    //   $message->setMessage("Informe a senha.", "error", "back");
+    // }else if(!preg_match('/^[0-9a-z]{8,12}$/i', $password)) {
+    //   $message->setMessage("Senha inválida.", "error", "back");
     // }
     /* 
       Pode conter letras e números
@@ -62,17 +65,17 @@
     */
 
     // validando senha permitindo caracteres, números e letras
-    if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $password)) {
-      $message->setMessage(
-        "
-        A senha não atende aos requisitos!
-        No mínimo têm que conter letras e números.
-        Deve conter pelo menos 1 número e 1 letra Maiúscula.
-        Pode conter qualquer um desses caracteres: !@#$%.
-        Deve ter de 8 até 12 caracteres.
-        ", "error", "back"
-      );
-    }
+    // if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $password)) {
+    //   $message->setMessage(
+    //     "
+    //     A senha não atende aos requisitos!
+    //     No mínimo têm que conter letras e números.
+    //     Deve conter pelo menos 1 número e 1 letra Maiúscula.
+    //     Pode conter qualquer um desses caracteres: !@#$%.
+    //     Deve ter de 8 até 12 caracteres.
+    //     ", "error", "back"
+    //   );
+    // }
 
   }else if($type === "login") {
 
