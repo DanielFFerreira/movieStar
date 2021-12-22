7c3de6a334fcd4fe712a32b01c734131bebbfd7f
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-12-22 20:24:40 -0300

    criando menu

--- templates/header.php
+++ templates/header.php
@@ -66,13 +66,28 @@
         <ul class="navbar-nav">
 
           <?php if($userData): ?>
-            <p>Está logado</p>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link">
+                <i class="far fa-plus-square"></i> Incluir Filme
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Meus Filmes</a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold">
+                <!-- <?= $userData->name ?> -->
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>logout.php" class="nav-link btn btn-danger">Sair</a>
+            </li>
           <?php else: ?>
             <li class="nav-item">
               <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
             </li>
           <?php endif; ?>
-          
+
         </ul>
       </div>
     </nav>
